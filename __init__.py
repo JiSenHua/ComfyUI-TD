f75b5d0ba6a7bf92c245f982ea03c87f9de9b04e
基森 <107310147+JiSenHua@users.noreply.github.com>
2025-02-26 17:29:28 +0800

    Update __init__.py
    add new class

--- __init__.py
+++ __init__.py
@@ -1,11 +1,17 @@
-from .node import Hy3DtoTD, ImagetoTD 
+from .node import *
 
 NODE_CLASS_MAPPINGS = {
+    "Comfy3DPacktoTD": Comfy3DPacktoTD,
     "Hy3DtoTD": Hy3DtoTD,
     "ImagetoTD": ImagetoTD,
+    "Tripo3DtoTD": Tripo3DtoTD,
+    "LoadTDImage": LoadTDImage,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
+    "Comfy3DPacktoTD": "Comfy3DPacktoTD",
     "Hy3DtoTD": "Hy3DtoTD",
-    "ImagetoTD": "ImagetoTD", 
+    "ImagetoTD": "ImagetoTD",
+    "Tripo3DtoTD": "Tripo3DtoTD",
+    "LoadTDImage": "LoadTDImage",
 }
